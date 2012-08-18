a8f69b0e5b77e6bf9b35d5c8c680b957c4cce176
Dan Goldin <dangoldin@gmail.com>
2012-08-18 12:07:45 -0400

    Added script to get include/extends hierarchy for django projects

---
+++ django_template_hierarchy.py
@@ -0,0 +1,68 @@
+import os
+import re
+import subprocess
+import pprint
+from collections import defaultdict
+
+root = os.path.realpath(os.path.dirname(__file__))
+
+RE_FILE = re.compile(r'[\'\"](.+?)[\"\']')
+
+def create_tree(parent, pairs):
+    tree = {}
+    for i,j in pairs:
+        if j == parent:
+            tree[i] = create_tree(i, pairs)
+    return tree
+
+def create_tree_multiple(parents, pairs):
+    tree = {}
+    for parent in parents:
+        tree[parent] = create_tree(parent,pairs)
+    return tree
+
+def get_file_pairs(command):
+    pairs = []
+    out = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE).communicate()[0]
+    for l in out.split("\n"):
+        data = l.split(':')
+        if len(data) == 2:
+            filename = data[0]
+            m = RE_FILE.search(data[1])
+            if m:
+                match = m.group(1)
+
+                filename = os.path.relpath(os.path.realpath(os.path.join(root, filename)))
+                match = os.path.relpath(os.path.realpath(os.path.join(root, 'templates', match)))
+
+                pairs.append((filename,match))
+
+    return pairs
+
+def child_nodes(pairs):
+    children = set()
+    for i,j in pairs:
+        children.add(i)
+    print 'Children:',children
+    return children
+
+def parent_nodes(pairs):
+    parents = set()
+    children = child_nodes(pairs)
+    for i,j in pairs:
+        if j not in children:
+            parents.add(j)
+    print 'Parents:',parents
+    return parents
+
+extend_pairs = get_file_pairs('find . -name "*.html" -exec grep -Hi "{% extends" {} \;')
+extend_parents = parent_nodes(extend_pairs)
+extend_tree = create_tree_multiple(extend_parents, extend_pairs)
+print 'Extends:', pprint.pformat(extend_tree, indent=1)
+
+# Need to switch the order for these so we see the file that's calling the include first
+include_pairs = [(j,i) for i,j in get_file_pairs('find . -name "*.html" -exec grep -Hi "{% include" {} \;')]
+#include_pairs = get_file_pairs('find . -name "*.html" -exec grep -Hi "{% include" {} \;')
+include_parents = parent_nodes(include_pairs)
+include_tree = create_tree_multiple(include_parents, include_pairs)
+print 'Includes:', pprint.pformat(include_tree, indent=1)
